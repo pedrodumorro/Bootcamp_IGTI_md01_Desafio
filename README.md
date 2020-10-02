c982092b64e5917a56becae5ba03b7735ff81280
Pedro Castro <pa.coelho.castro@gmail.com>
2020-10-01 23:29:14 -0300

    commit #1

---
+++ README.md
@@ -0,0 +1,17 @@
+<h1 align="center">Bootcamp IGTI "Desenvolvedor Full-Stack" - Módulo 1 - Desafio</h1>
+
+<p align="justify">O intuito desse desafio é aplicar os conceitos aprendidos na segunda semana de aulas, por exemplo:
+
+ - Manipulação de arrays com métodos map, filter e reduce;
+ - Formatação de valores com JavaScript;
+ - Mapeamento de elementos do DOM para manipulação com JavaScript;
+ - e realização de requisições HTTP com comando fetch()
+
+para implantar uma aplicação de filtragem de usuários e cálculos de estatísticas relacionadas aquele filtro.
+
+A idéia inicial era não preocupar muito com a interface da aplicação, focando apenas na funcionalidade da aplicação.
+
+Na verdade não utilizei nem os métodos principais de manipulação de arrays como indicado, mas irei realizar a refatoração assim que possível.
+</p>
+
+> Status do projeto: Concluído :heavy_check_mark:
---
+++ js/script.js
@@ -0,0 +1,118 @@
+let searchInput, searchBtn, usersList, statistics, totalAges = 0, avgAges = 0, male = 0, female = 0, termSearched = '', userArray = [], usersFiltered = []
+
+window.addEventListener('load', () => {
+  searchInput = document.querySelector('#searchInput')
+  searchBtn = document.querySelector('#searchBtn')
+  usersList = document.querySelector('#usersList')
+  statistics = document.querySelector('#searchStatistics')
+
+  fetchUsers()
+})
+
+const fetchUsers = async () => {
+  const promiseOfUsers = await fetch('https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo')
+  const jsonOfUsers = await promiseOfUsers.json()
+  const usersListFull = jsonOfUsers.results
+
+  usersListFull.forEach(user => {
+    userArray.push(
+      {
+        name: `${user.name.first} ${user.name.last}`,
+        picture: `${user.picture.thumbnail}`,
+        age: `${user.dob.age}`,
+        gender: `${user.gender}`
+      }
+    )
+  });
+  searchInput.addEventListener('keyup', searchedTerm)
+  searchBtn.addEventListener('click', searchedTerm)
+
+  render()
+}
+
+const searchedTerm = (e) => {
+  usersFiltered = []
+
+  if (searchInput.value !== '') {
+    searchBtn.disabled = false
+  }
+  else {
+    searchBtn.disabled = true
+  }
+
+  if (e.key === 'Enter' || e.type === 'click') {
+    termSearched = searchInput.value
+
+    if (termSearched.length === 0) {
+      render()
+    }
+    else {
+      userArray.forEach(user => {
+        let name = user.name.toLowerCase()
+        if (name.includes(termSearched.toLowerCase())) {
+          usersFiltered.push(user)
+        }
+      })
+      render()
+    }
+  }
+}
+
+const render = () => {
+  if (usersFiltered.length === 0) {
+    usersList.innerHTML = '<div><h2>Nenhum usuário filtrado</h2></div>'
+    statistics.innerHTML = '<div><h2>Nada a ser exibido</h2></div>'
+  }
+  else {
+    renderList();
+    renderStatistics();
+  }
+}
+
+const renderList = () => {
+  let usersHTML = "<ul>"
+  usersFiltered.forEach(user => {
+    let userHTML =
+      `
+      <li class='user'>
+        <div class='picture'><img src='${user.picture}'/></div>
+        <div class='name'><span>${user.name},</span></div>
+        <div class='age'><span>${user.age} Anos</span></div>
+      </li>
+    `
+    usersHTML += userHTML
+  })
+  usersList.innerHTML = usersHTML
+}
+
+const renderStatistics = () => {
+  const genderCount = () => {
+    statistics.innerHTML = ''
+    male = usersFiltered.filter(user => user.gender === 'male').length
+    female = usersFiltered.filter(user => user.gender === 'female').length
+    const genderHTML =
+      `
+    <p>Sexo Masculino: <strong>${male}</strong></p>
+    <p>Sexo Feminino: <strong>${female}</strong></p>
+    `
+    statistics.innerHTML += genderHTML
+  }
+
+  const sumTotalAges = () => {
+    usersFiltered.map(user => {
+      totalAges += +(user.age)
+    })
+    const agesHTML = `<p>Soma das idades: <strong>${totalAges}</strong></p>`
+    statistics.innerHTML += agesHTML
+  }
+
+  const averageAges = () => {
+    avgAges = (totalAges / (female + male)).toFixed(2)
+    const avgAgesHTML = `<p>Média das idades: <strong>${avgAges}</strong></p>`
+    statistics.innerHTML += avgAgesHTML
+  }
+
+  genderCount()
+  sumTotalAges()
+  averageAges()
+}
